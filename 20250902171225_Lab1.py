ceea2463dc5866dace8287a2d07873baad400092
JustinKhairy <6307048@edu.vaniercollege.qc.ca>
2025-09-02 17:15:05 -0400

    Add files via upload

---
+++ 20250902171225_Lab1.py
@@ -0,0 +1,27 @@
+# Make sure you are not modifying the method signatures
+# Function 1: Write a simple Hello World program
+# This function should print "Hello, World!" to the screen.
+def hello_world():
+    # TODO: Implement this function
+    print("hello world!")
+    pass  # Replace with your code
+    
+
+# Function 2: Get input and output with different variable types
+# This function should prompt the user for their name (string), age (int), and height (float),
+# and then print them back in a formatted message.
+def input_output():
+    # TODO: Implement this function
+   name= str(input("enter your name:"))
+   age=int(input("enter your age:"))
+   height=float(input("enter your height:"))
+   print("Hello, "+name+"")
+   print("you are "+str(age)+" years old")
+   print("you are "+str(height)+" meters tall")
+   pass  # Replace with your code
+
+
+
+hello_world()
+input_output()
+
